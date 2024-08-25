1cc00603939c96ab67e03944330dc62078f09d6e
Youngseok Lee <youngseoklee@Youngseoks-MacBook-Air.local>
2024-08-25 14:05:45 -0400

    added form validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,20 +1,22 @@
 'use client';
 import React, {useState} from 'react';
-import {Button, Callout, TextField} from "@radix-ui/themes";
+import {Button, Callout, TextField, Text} from "@radix-ui/themes";
 import {Controller, useForm} from "react-hook-form";
 import SimpleMDE from 'react-simplemde-editor';
 import "easymde/dist/easymde.min.css";
 import axios from "axios";
-import {useRouter} from "next/navigation";
+import { useRouter } from "next/navigation";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from "@/app/issues/validationSchema";
+import { z } from 'zod';
 
-interface NewIssueForm {
-    title: string;
-    description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>
 
 const NewIssuePage = () => {
-    const { register, control, handleSubmit}
-        = useForm<NewIssueForm>();
+    const { register, control, handleSubmit,
+        formState: { errors } } = useForm<IssueForm>({
+        resolver: zodResolver(createIssueSchema)
+    });
     const [error, setError] = useState('');
 
     const router = useRouter();
@@ -40,6 +42,7 @@ const NewIssuePage = () => {
                 <TextField.Root>
                     <TextField.Input placeholder={"Enter issue title"} {...register('title')}/>
                 </TextField.Root>
+                {errors.title && <Text color="red" as={'p'}>{errors.title.message}</Text>}
                 <Controller
                     name="description"
                     control={control}
@@ -47,6 +50,7 @@ const NewIssuePage = () => {
                         <SimpleMDE placeholder="Description" {...field} />
                     )}
                 />
+                {errors.description && <Text color="red" as={'p'}>{errors.description.message}</Text>}
                 <Button>Submit New Issue</Button>
             </form>
         </div>
